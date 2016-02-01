212100f079d1b177d90be7ee56820bdd55c3475a
Joe Oldenburg <joseph.oldenburg@gmail.com>
2016-02-01 12:21:03 -0700

    one time commit for oauth js

---
+++ oauth.js
@@ -0,0 +1,14 @@
+var ids = {
+  facebook: {
+    clientID: '216208822056820',
+    clientSecret: '69ae0eedb973b45dc910a087e1fe8aa4',
+    callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+  },
+  google: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
+  }
+};
+
+module.exports = ids;
